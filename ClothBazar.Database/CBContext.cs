e6de60fd9070f477e29fedf5a72317f202a50718
Shubham Kulkarni <kulkarnishubham587@gmail.com>
2022-10-07 20:40:34 +0530

    db

---
+++ ClothBazar.Database/CBContext.cs
@@ -0,0 +1,22 @@
+﻿using ClothBazar.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClothBazar.Database
+{
+    public class CBContext : DbContext
+    {
+        public CBContext() : base()
+        {
+           
+        }
+
+        public DbSet<Product> products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+    }
+}
