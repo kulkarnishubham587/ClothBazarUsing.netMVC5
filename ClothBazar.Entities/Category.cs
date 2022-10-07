bf5558c73d56908efd25e3204263769a13629a1f
Shubham Kulkarni <kulkarnishubham587@gmail.com>
2022-10-07 17:46:31 +0530

    Add project files.

---
+++ ClothBazar.Entities/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClothBazar.Entities
+{
+    public class Category:BaseEntity
+    {
+     
+        //relationship
+        public List<Product> Products { get; set; }
+    }
+}
---
+++ ClothBazar.Entities/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClothBazar.Entities
+{
+    public class Product:BaseEntity
+    {
+    
+        public double Price { get; set; }
+
+        //relationship
+        public Category Category { get; set; }
+
+    }
+}
