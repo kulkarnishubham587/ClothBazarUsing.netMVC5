e90b28e9412a55e0c2b5d78a399a611efdebe870
Shubham Kulkarni <kulkarnishubham587@gmail.com>
2022-10-13 18:36:25 +0530

    updates till 13-10

---
+++ ClothBazar.Service/ProductsService.cs
@@ -0,0 +1,45 @@
+﻿using ClothBazar.Database;
+using ClothBazar.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ClothBazar.Service
+{
+    public class ProductsService
+    {
+        CBContext db = new CBContext();
+        public void SaveProduct(Product product)
+        {
+            db.products.Add(product);
+            db.SaveChanges();
+        }
+
+        public List<Product> ShowAllProducts()
+        {
+            return db.products.ToList();
+        }
+
+
+        public Product GetById(int id)
+        {
+            return db.products.Find(id);
+        }
+
+        public void EditProduct(Product product)
+        {
+            db.Entry(product).State = System.Data.Entity.EntityState.Modified;
+            db.SaveChanges();
+        }
+
+        public void Delete(Product product)
+        {
+            //db.Entry(category).State = System.Data.Entity.EntityState.Deleted; 
+            db.products.Remove(product);
+            db.SaveChanges();
+        }
+    }
+}
---
+++ Eshop/Controllers/ProductController.cs
@@ -0,0 +1,43 @@
+﻿using ClothBazar.Entities;
+using ClothBazar.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Eshop.Controllers
+{
+    public class ProductController : Controller
+    {
+        ProductsService productsService = new ProductsService();
+        // GET: AllProduct
+        public ActionResult Index()
+        {
+            return View( );
+        }
+
+
+        public ActionResult ProductTable()
+        {
+            var products = productsService.ShowAllProducts();
+            return PartialView(products);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+
+            return PartialView();
+        }
+
+        //create Category
+        [HttpPost]
+        public ActionResult Create(Product product)
+        {
+            productsService.SaveProduct(product);
+            return RedirectToAction("ProductTable");
+        }
+ 
+    }
+}
